9ea8fa0a44a5184721bac2f96e14f9bb2d65d255
Tzezo <tzvetomir.krastev@gmail.com>
2017-08-20 23:25:54 +0000

    Added packeges.sh

---
+++ setup/packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get install libdbd-pg-perl
+apt-get install libfile-slurp-perl
+apt-get install libtry-tiny-perl
+
+apt-get install libtest-exception-perl
+apt-get install libfile-type-perl
+apt-get install libmime-types-perl
+apt-get install libtest-mockmodule-perl
+apt-get install libtest-mockobject-perl
+apt-get install libjson-perl
